aaf9e56e39102d5c3d5b5eedfdabeb424f53c6c0
Your Name <you@example.com>
2022-06-23 05:01:47 +0530

    harmonic series

--- src/day5/Day5_problems.java
+++ src/day5/Day5_problems.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class Day5_problems {
 
 	public static void main(String[] args) {
+		float ans=0;
 		System.out.println("enter the number");
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		for (int i=1; i<=n; i++) {
-			System.out.println(+Math.pow(2, i));
+			ans += (float)1/i;
 		}
+		System.out.println(+ans);
 	}
 
 }
