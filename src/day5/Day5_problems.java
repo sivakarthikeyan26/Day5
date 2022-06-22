7bf431ed3b1bfc83f63905798c7db1b92e84092f
Your Name <you@example.com>
2022-06-23 04:34:26 +0530

    leap year

--- src/day5/Day5_problems.java
+++ src/day5/Day5_problems.java
@@ -5,24 +5,14 @@ import java.util.Scanner;
 public class Day5_problems {
 
 	public static void main(String[] args) {
-		int TailCounter=0;
-		int HeadCounter=0;
-		System.out.println("Enter no of times to flip the coin");
+		System.out.println("enter the four digit year");
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for (int i=1 ; i<=n ; i++) {
-			float a = (float) (Math.random());
-			double coin = (double) Math.round(a * 10) / 10;
-			if(coin<0.5) {
-				TailCounter +=1;
-			}
-			else {
-				HeadCounter +=1;
-			}
-		}
-		float tail_percent= (float)TailCounter/n *100;
-		float head_percent= (float) (100-tail_percent);
-		System.out.println("tail percentage is :"+tail_percent+" and head percentage is :"+head_percent);
+		int year = sc.nextInt();
+
+	     if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+	       System.out.println (year + " is a Leap Year");
+	     else
+	         System.out.println (year + " is not a Leap Year");
 	}
 
 }
