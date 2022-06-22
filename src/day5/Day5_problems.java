bac0f16d31b4b0f8e3420347f1bb85887d0b8cd1
Your Name <you@example.com>
2022-06-23 05:20:53 +0530

    primfactors

--- src/day5/Day5_problems.java
+++ src/day5/Day5_problems.java
@@ -5,14 +5,23 @@ import java.util.Scanner;
 public class Day5_problems {
 
 	public static void main(String[] args) {
-		float ans=0;
+		int count =0;
 		System.out.println("enter the number");
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		for (int i=1; i<=n; i++) {
-			ans += (float)1/i;
+			count=0;
+			if (n%i==0) {
+				for (int j=1; j<=i; j++) {
+					if(i%j==0) {
+						count += 1;
+					}
+				}
+			}
+			if (count==2 || count==1) {
+				System.out.println(+i);
+			}
 		}
-		System.out.println(+ans);
 	}
 
 }
