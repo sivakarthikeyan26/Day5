32f223e0ac25af1d7be8db85b6e89af79facde73
Your Name <you@example.com>
2022-06-23 05:40:35 +0530

    swapping of numbers

--- src/day5/Day5_problems.java
+++ src/day5/Day5_problems.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class Day5_problems {
 
 	public static void main(String[] args) {
-		float quotient=0;
-		int remainder=0;
+		int temp;
 		System.out.println("enter two numbers");
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		quotient=(float)a/b;
-		remainder=a%b;
-		System.out.println("The quotient is :"+quotient+" and the remainder is:"+remainder);
-		
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("the value of a is :"+a+" and b is :"+b);
 	}
 
 }
