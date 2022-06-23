d21413a1fb1d83a3d8399b18cf1045a9a1f78536
Your Name <you@example.com>
2022-06-23 05:43:15 +0530

    odd or even

--- src/day5/Day5_problems.java
+++ src/day5/Day5_problems.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Day5_problems {
 
 	public static void main(String[] args) {
-		int temp;
-		System.out.println("enter two numbers");
+		System.out.println("enter the numbers");
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		int b = sc.nextInt();
-		temp=a;
-		a=b;
-		b=temp;
-		System.out.println("the value of a is :"+a+" and b is :"+b);
+		if(a%2==0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+		
 	}
-
 }
